76cbf63cf4599c7cc305bf67619edf565436131c
Nhi Nguyen <70039953+Nhinguyen0506@users.noreply.github.com>
2025-04-15 16:05:52 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Nhi_Nguyen_PictureBox
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.picAutoSize = new System.Windows.Forms.PictureBox();
+            this.picCenter = new System.Windows.Forms.PictureBox();
+            this.picNormal = new System.Windows.Forms.PictureBox();
+            this.picZoom = new System.Windows.Forms.PictureBox();
+            this.picStretch = new System.Windows.Forms.PictureBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picAutoSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCenter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNormal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picZoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStretch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Normal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(281, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Center Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(583, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Auto Size";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Stretch Image";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(281, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Zoom";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(551, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Size";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // picAutoSize
+            // 
+            this.picAutoSize.Location = new System.Drawing.Point(586, 63);
+            this.picAutoSize.Name = "picAutoSize";
+            this.picAutoSize.Size = new System.Drawing.Size(334, 193);
+            this.picAutoSize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picAutoSize.TabIndex = 6;
+            this.picAutoSize.TabStop = false;
+            // 
+            // picCenter
+            // 
+            this.picCenter.Location = new System.Drawing.Point(284, 63);
+            this.picCenter.Name = "picCenter";
+            this.picCenter.Size = new System.Drawing.Size(198, 159);
+            this.picCenter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picCenter.TabIndex = 7;
+            this.picCenter.TabStop = false;
+            // 
+            // picNormal
+            // 
+            this.picNormal.Location = new System.Drawing.Point(12, 63);
+            this.picNormal.Name = "picNormal";
+            this.picNormal.Size = new System.Drawing.Size(187, 159);
+            this.picNormal.TabIndex = 8;
+            this.picNormal.TabStop = false;
+            // 
+            // picZoom
+            // 
+            this.picZoom.Location = new System.Drawing.Point(284, 307);
+            this.picZoom.Name = "picZoom";
+            this.picZoom.Size = new System.Drawing.Size(198, 190);
+            this.picZoom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picZoom.TabIndex = 9;
+            this.picZoom.TabStop = false;
+            // 
+            // picStretch
+            // 
+            this.picStretch.Location = new System.Drawing.Point(12, 307);
+            this.picStretch.Name = "picStretch";
+            this.picStretch.Size = new System.Drawing.Size(187, 190);
+            this.picStretch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picStretch.TabIndex = 10;
+            this.picStretch.TabStop = false;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(586, 440);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(163, 35);
+            this.btnSelect.TabIndex = 11;
+            this.btnSelect.Text = "Select Image";
+            this.btnSelect.UseVisualStyleBackColor = false;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(812, 440);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(117, 35);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(586, 345);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(162, 22);
+            this.txtSize.TabIndex = 13;
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 509);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.picStretch);
+            this.Controls.Add(this.picZoom);
+            this.Controls.Add(this.picNormal);
+            this.Controls.Add(this.picCenter);
+            this.Controls.Add(this.picAutoSize);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nhi_Nguyen_PictureBox";
+            this.Load += new System.EventHandler(this.frmMainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picAutoSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCenter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNormal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picZoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStretch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox picAutoSize;
+        private System.Windows.Forms.PictureBox picCenter;
+        private System.Windows.Forms.PictureBox picNormal;
+        private System.Windows.Forms.PictureBox picZoom;
+        private System.Windows.Forms.PictureBox picStretch;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nhi_Nguyen_PictureBox
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+        int originalWidth;
+        int originalHeight;
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            // Reset form size to original
+            picAutoSize.Width = picNormal.Width;
+            picAutoSize.Height = picNormal.Height;
+            Width = originalWidth; 
+            Height = originalHeight;
+
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                Image selectedImage = Image.FromFile(dlgOpen.FileName);
+                picNormal.Image = selectedImage;
+                picCenter.Image = selectedImage;
+                picAutoSize.Image = selectedImage;
+                picStretch.Image = selectedImage;
+                picZoom.Image = selectedImage;
+                txtSize.Text = picAutoSize.Width.ToString() + "x" + picAutoSize.Height.ToString();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmMainForm_Load(object sender, EventArgs e)
+        {
+            Width = originalWidth;
+            Height = originalHeight;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
